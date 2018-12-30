de663f7be69bf51e66393e23741c9aa61d27b769
Viktor Smari <viktorsmari@gmail.com>
2018-12-30 15:32:14 +0100

    Table sorting

--- src/DeviceResults.js
+++ src/DeviceResults.js
@@ -10,7 +10,7 @@ class DeviceResults extends Component{
       id: 'id',
       Header: 'ID',
       accessor: 'id', // String-based value accessors!
-      Cell: e => <div style={{color: 'blue', textDecoration: 'underline', cursor: 'pointer'}} 
+      Cell: e => <div style={{color: 'blue', textDecoration: 'underline', cursor: 'pointer'}}
         onClick={() => this.props.changeSelectedDevice(e.value)} > {e.value} </div>
     }, {
       Header: 'Last Reading',
@@ -22,50 +22,24 @@ class DeviceResults extends Component{
     }, {
       Header: 'City',
       id: 'city',
-      accessor: d => d.owner.location.city,
+      accessor: 'owner.location.city'
     }, {
       Header: 'Username',
       id: 'username',
-      accessor: d => d.owner.username,
+      accessor: 'owner.username',
     }, {
       Header: 'Name',
       accessor: 'name' // String-based value accessors!
     }]
 
     return(
-      <div className="row">
+      <div className="row mb-2">
         <div className="col-12">
-        <ReactTable
-        data={item}
-        columns={columns}
-        minRows={2} />
           <h3>Devices</h3>
-          <table className="table table-sm table-bordered table-hover table-striped table-responsive">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Sensors</th>
-                <th>Last Reading</th>
-                <th>Name</th>
-                <th>Username</th>
-                <th>City</th>
-              </tr>
-            </thead>
-            <tbody>
-              {item.map((x,y) => {
-                return(
-                  <tr key={y} className="tr" onClick={() => this.props.changeSelectedDevice(x['id'])} >
-                    <td><p>{x['id']} </p></td>
-                    <td><p>{x['data']['sensors'].length}</p></td>
-                    <td><p>{x['last_reading_at']}</p></td>
-                    <td><p>{x['name']}</p></td>
-                    <td><p>{x['owner'] ? x['owner']['username'] : ''}</p></td>
-                    <td><p>{x['owner'] ? x['owner']['location']['city'] : ''}</p></td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
+          <ReactTable
+          data={item}
+          columns={columns}
+          minRows={1} />
         </div>
       </div>
     )
